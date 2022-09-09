cecf1cb4fd74365b63484862b15fc7d35ba7aaac
ronnycyy <m17722548276@163.com>
2022-09-09 23:30:08 +0800

    update project .md

--- projects/project-cmp/demos/upload-big-file-demo/demos/demo1/client/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
-import UpLoadFile from './Upload';
-
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(<UpLoadFile />);
